46d11cd39f0591510d8b770f5cfe5400256eb27d
Aryan Gupta <aryan9090900@gmail.com>
2025-11-03 00:14:35 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,16 +8,16 @@ const Index = () => {
     <div className="h-screen bg-background flex flex-col">
       
       
-      <main className="flex-1 pt-2 grid lg:grid-cols-[2fr_1fr] overflow-hidden">
+      <main className="flex-1 pt-2  overflow-auto3">
         {/* Left Panel */}
         <div className=" px-8 pb-8 space-y-8 w-full">
           <LeftPanel />
         </div>
 
         {/* Leaderboard */}
-        <div className="overflow-y-auto border-l border-border px-4 pb-8">
+        {/* <div className="overflow-y-auto border-l border-border px-4 pb-8">
           <Leaderboard />
-        </div>
+        </div> */}
       </main>
     </div>
   );
